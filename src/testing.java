352fec5c0e11bb6d933b7b869fa3c64854278e4a
Anthony Nguyen <anthonyqn@comcast.net>
2018-01-06 09:42:11 -0500

    testing

--- src/testing.java
+++ src/testing.java
@@ -4,7 +4,7 @@
 
 /**
  * @author Anthony
- *
+ * Testing if I'm actually doing this somewhat correctly
  */
 public class testing {
 
@@ -13,7 +13,13 @@ public class testing {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("I really hope I did this correctly");
+		System.out.println("I really hope that I'm doing this correctly");
+/**
+ * Why are there no unstaged changes showing up??
+ * This is all rather unfortunate
+ * so I had to hit save		
+ */
 	}
 
 }
